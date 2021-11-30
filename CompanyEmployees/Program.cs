c853ec65e81a790ea21a90d91934582b7d031db6
Victor Bassey <viktorbasi545@gmail.com>
2021-11-30 10:34:31 +0200

    Configure logging

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,11 +1,16 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 
 // Add services to the container.
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 builder.Services.AddControllers();
 
 var app = builder.Build();

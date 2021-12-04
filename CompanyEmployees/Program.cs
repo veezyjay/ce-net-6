903e3e0f9aea4cd573ca597ce9f6842ec5e7f9b6
Victor Bassey <viktorbasi545@gmail.com>
2021-12-04 10:50:41 +0200

    Refactor to use DTOs and automapper

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -16,6 +16,7 @@ builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 

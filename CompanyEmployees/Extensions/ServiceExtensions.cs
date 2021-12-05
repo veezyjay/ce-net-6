c00a0709bc0445a25c33f571c8f12844048d27b0
Victor Bassey <viktorbasi545@gmail.com>
2021-12-05 08:09:36 +0200

    Implement content negotiation

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Contracts;
+﻿using CompanyEmployees.Formatters;
+using Contracts;
 using LoggerService;
 using Microsoft.EntityFrameworkCore;
 using Repository;
@@ -36,5 +37,9 @@ namespace CompanyEmployees.Extensions
         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) => 
             services.AddDbContext<RepositoryContext>(opts => 
                 opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) => 
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
+
     }
 }

6560dfa1cd45c61cd2eeec397d550b38050efb06
Victor Bassey <viktorbasi545@gmail.com>
2021-12-05 10:12:57 +0200

    Add endpoints for creating companies and employees

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -9,6 +9,8 @@ namespace Repository
         {
         }
 
+        public void CreateCompany(Company company) => Create(company);
+
         public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
                 FindAll(trackChanges)
                 .OrderBy(c => c.Name)
@@ -17,6 +19,10 @@ namespace Repository
         public Company GetCompany(Guid companyId, bool trackChanges) => 
             FindByCondition(c => c.Id.Equals(companyId), trackChanges)
             .SingleOrDefault();
+
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) => 
+            FindByCondition(x => ids.Contains(x.Id), trackChanges)
+            .ToList();
 
     }
 }
